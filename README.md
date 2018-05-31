5eaa696847f3ef6c445901cc89dd427d21e18818
Karen Vicente <karenyasmin37@gmail.com>
2018-05-30 21:18:49 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Chat NodeJS
+# Chat NodeJS :speech_balloon:
 
 Este repositório contém um projeto Chat feito com NodeJS.
+
+![alt text](app.jpg)
+
+## Características
+- [Socket.io](https://socket.io) para comunicação
+- [Express](http://expressjs.com/pt-br/) Framework
+- [EJS](http://ejs.co) Template Node 
+- [Bootstrap 4](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
+
+## Rodando Aplicação
+
+1. Instalar as dependências com npm:
+`npm install`
+
+2. Subir o servidor via NodeJS, no terminal executar:
+`node server.js`
+
+> Dica: teste com mais de uma pessoa conectada!! ou abra duas abas no seu navegador rs
+
+Abrir navegador em http://localhost:3000/.
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app); //define protocolo HTTP
+const io = require('socket.io')(server); //config web socket
+
+app.use(express.static(path.join(__dirname, 'public'))); //path frontend da aplicação
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => { //definindo a rota inicial
+    res.render('index.html');
+});
+
+let messages = [];
+
+/**
+ * - Toda vez que algum cliente se conectar ele vai receber o socket
+ */
+io.on('connection', socket => {
+    console.log(`Socket conctado: ${socket.id}`);
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+})
+
+server.listen(3000); //porta de escuta
